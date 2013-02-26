c39c636bbbfd6c7136d55231b8f3ea15e299ea92
shasan <auvi.2k5@gmail.com>
2013-02-26 07:04:22 -0600

    model 'Gene' modified

---
+++ gsite/grelation/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from grelation.models import Gene
+
+admin.site.register(Gene)
--- gsite/grelation/models.py
+++ gsite/grelation/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 class Gene(models.Model):
 	name = models.TextField()
-	gid = models.IntegerField()
-	duplicate = models.BooleanField()
+	gid = models.BigIntegerField()
+	is_symbol = models.BooleanField()
+	is_gname = models.BooleanField()
+	is_duplicate = models.BooleanField()
 	
 	def __unicode__(self):
-		return self.name, self.gid
+		return self.name
