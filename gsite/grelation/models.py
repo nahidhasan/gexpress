b9c6ca357282312f07de54280e8ef69900090ceb
shasan <auvi.2k5@gmail.com>
2013-02-26 05:38:54 -0600

    model 'Gene' created.

--- gsite/grelation/models.py
+++ gsite/grelation/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Gene(models.Model):
+	name = models.TextField()
+	gid = models.IntegerField()
+	duplicate = models.BooleanField()
+	
+	def __unicode__(self):
+		return self.name, self.gid
